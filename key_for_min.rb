e4792fefe71cff6e9345af825a9116a9cbc71c5f
Natalie Taktachev <ntaktach@gmail.com>
2019-07-21 22:45:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   last_value = 0
+  last_key = 
   name_hash.each do |key, value|
     if last_value < value
       last_value = value
-      puts key
+      last_key = key
     end
   end
   #puts last_value
